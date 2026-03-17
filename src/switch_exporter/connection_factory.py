638d3a3ca53c6ecf053b43fc317f153602eafa6f
Eaton Emmerich <e.e.metabee@gmail.com>
2026-03-17 14:49:02 +0000

    semaphore for safe simultaneous closing of connection

--- src/switch_exporter/connection_factory.py
+++ src/switch_exporter/connection_factory.py
@@ -6,6 +6,8 @@ from typing import Callable, Dict, Union
 import asyncssh
 logger = logging.getLogger(__name__)
 
+MAX_CONNECTION_COUNT = 5  # max simultaneous connections to a switch
+
 
 class Connection:
     def __init__(self, hostname: str, username: str, password: str, keyfile: str, on_close: Callable[[], None]) -> None:
@@ -14,35 +16,39 @@ class Connection:
         self.password = password
         self.keyfile = keyfile
         self.conn = None  # type: Union[asyncssh.SSHClientConnection, None]
-        self._lock = asyncio.Lock()
+        self._semaphore = asyncio.Semaphore(MAX_CONNECTION_COUNT)
         self.on_close = on_close
 
     async def run_process(self, command: str) -> Union[bytes, str, None]:
         """Get a process from the connection."""
-        async with self._lock:
+        async with self._semaphore:
             if self.conn is None:
                 self.conn = await asyncssh.connect(
                     self.hostname, username=self.username, password=self.password, client_keys=self.keyfile, known_hosts=None
                 )
 
             logger.debug('Running command %s', command)
-        completed_process = await self.conn.run(command=None, input=command)
-        if completed_process.returncode != 0:
-            logger.error(
-                '[%s] Error running command %s: return code %s',
-                self.hostname, command, completed_process.returncode
-            )
-            logger.debug('[%s] Stdout: %s', self.hostname, completed_process.stdout)
-            logger.debug('[%s] Stderr: %s', self.hostname, completed_process.stderr)
-        return completed_process.stdout
+
+            completed_process = await self.conn.run(command=None, input=command)
+            if completed_process.returncode != 0:
+                logger.error(
+                    '[%s] Error running command %s: return code %s',
+                    self.hostname, command, completed_process.returncode
+                )
+                logger.debug('[%s] Stdout: %s', self.hostname, completed_process.stdout)
+                logger.debug('[%s] Stderr: %s', self.hostname, completed_process.stderr)
+            return completed_process.stdout
 
     async def close(self) -> None:
-        async with self._lock:
-            if self.conn is not None:
-                self.conn.close()
-                await self.conn.wait_closed()
-                self.conn = None
-                self.on_close()
+        for _ in range(MAX_CONNECTION_COUNT):
+            await self._semaphore.acquire()
+        if self.conn is not None:
+            self.conn.close()
+            await self.conn.wait_closed()
+            self.conn = None
+            self.on_close()
+        for _ in range(MAX_CONNECTION_COUNT):
+            self._semaphore.release()
 
 
 class ConnectionFactory:
